591af1e6ead17f23cc3b02e2e28baa792c8f0fcd
ashutoshjha23 <jashutosh498@gmail.com>
2023-08-14 02:38:03 +0530

    events

---
+++ Events/event.js
@@ -0,0 +1,11 @@
+var div = document.querySelector("div");
+var button=document.querySelector("button");
+
+div.addEventListener("click",()=>{
+    gittalert("div is clicked");
+}
+
+)
+button.addEventListener("click",()=>{
+    alert("button is clicked");
+})
