48c0bc0fe91ae0fad0a8b006e872e1b400b77524
ashutoshjha23 <jashutosh498@gmail.com>
2023-08-14 15:36:49 +0530

    event-propagation

--- Events/event.js
+++ Events/event.js
@@ -1,11 +1,27 @@
-var div = document.querySelector("div");
-var button=document.querySelector("button");
+const div=document.querySelector("div");
+const form=document.querySelector("form");
+const button=document.querySelector("button");
 
-div.addEventListener("click",()=>{
-    gittalert("div is clicked");
-}
+//event capturing/trickling : just add true---from parent to child
+//event.target //this.target //event.currentTarget
+//event bubbling: from child to parent
 
-)
-button.addEventListener("click",()=>{
-    alert("button is clicked");
-})
+div.addEventListener("click",function(){
+    alert("div is clicked");
+});
+form.addEventListener("click",function(){
+    alert("form is clicked");
+});
+
+//EVENT captuting
+button.addEventListener("click",function(){
+    alert("button is clicked")
+    ,{
+        capture:true,
+
+    }
+});
+
+//what is event delegation?
+//if there are multiple events to be triggerd then it that case we use event delegation
+//and apply event listener on the parent
